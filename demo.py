8662de8e7d3b05674d07bafcc9c93827f274e793
ZhuangPeiyu <863231064@qq.com>
2021-04-20 17:51:54 +0800

    Delete demo.py

--- demo.py
+++
@@ -1,6 +0,0 @@
-import os
-import tensorflow as tf
-FLAGS = tf.flags.FLAGS
-tf.flags.DEFINE_integer('filename', 'train.py', 'the file to run (train.py or test.py)')
-
-os.system("python3 ./"+FLAGS.filename)
